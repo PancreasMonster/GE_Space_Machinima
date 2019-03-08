22c3cc7567d5e1069916d0ef902e150c05ed35a7
PancreasMonster <ryanflood444@gmail.com>
2019-03-08 20:15:58 +0000

    Intro Scene

---
+++ Assets/Scripts/TypeWriter Text Effect.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeWriterTextEffect : MonoBehaviour
+{
+    public string[] letters;
+    public float delay;
+    public AudioSource aud;
+    private Text text;
+    private int currentLetter = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        StartCoroutine(nextLetter());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator nextLetter()
+    {
+        yield return new WaitForSeconds(delay);
+        text.text = text.text + letters[currentLetter];
+        currentLetter++;
+        if (aud != null)
+            aud.Play();
+        if (currentLetter > letters.Length)
+            StartCoroutine(nextLetter());
+    }
+}
---
+++ Assets/Scripts/Yes.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Yes : MonoBehaviour
+{
+    public string[] letters;
+    public float delay, pauseLenght;
+    public AudioSource aud;
+    public bool pause;
+    private Text text;
+    private int currentLetter = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        StartCoroutine(nextLetter());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator nextLetter()
+    {   
+        if (pause)
+        {
+            yield return new WaitForSeconds(pauseLenght);
+            pauseLenght = 0;
+        }
+
+        text.text = text.text + letters[currentLetter];
+        yield return new WaitForSeconds(delay);
+        currentLetter++;
+        if (aud != null && !aud.isPlaying)
+            aud.Play();
+        if (currentLetter < letters.Length)
+            StartCoroutine(nextLetter());
+        if (currentLetter == letters.Length && aud != null)
+        {
+            aud.Stop();
+        }
+    }
+}
