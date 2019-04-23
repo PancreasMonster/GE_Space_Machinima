8e375448fd6fb58b9fd081ffddc1703e0b6571a5
PancreasMonster <ryanflood444@gmail.com>
2019-04-23 13:16:57 +0100

    Small Changes

--- Assets/Scripts/BoidSpawner.cs
+++ Assets/Scripts/BoidSpawner.cs
@@ -7,11 +7,12 @@ public class BoidSpawner : MonoBehaviour
 
     public GameObject boidAgent;
     public List<GameObject> agents = new List<GameObject>();
+    public int num;
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < 10; i++)
+        for(int i = 0; i < num; i++)
         {
             GameObject clone = Instantiate(boidAgent, transform.TransformPoint(new Vector3(Random.Range(-1500, 1500), Random.Range(-1500, 1500), Random.Range(-1500, 1500))), Quaternion.identity);
             agents.Add(clone);
